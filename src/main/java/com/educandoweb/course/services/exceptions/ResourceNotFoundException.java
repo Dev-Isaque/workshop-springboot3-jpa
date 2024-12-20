a6190c2c593c940e31d4e62e2add282019293bd6
Isaque_Soares <isaquesbl123@gmail.com>
2024-12-19 21:49:32 -0300

    Exception handling - findById

--- src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/educandoweb/course/services/exceptions/ResourceNotFoundException.java
@@ -1,5 +1,12 @@
 package com.educandoweb.course.services.exceptions;
 
-public class ResourceNotFoundException {
-    
+public class ResourceNotFoundException extends RuntimeException {
+
+    @SuppressWarnings("unused")
+    private static final Long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(Object id) {
+        super("Resource not found. Id " + id);
+    }
+
 }
